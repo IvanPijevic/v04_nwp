15951763c5771722e99e14dc227813acdb6e71d3
Asfodel <ivan.pijevic@gmail.com>
2025-04-26 20:59:02 +0200

    ispravak

--- main.cpp
+++ main.cpp
@@ -16,14 +16,14 @@ protected:
     {
         if (!ship) 
         {
-            ship.create(*this, WS_CHILD | WS_VISIBLE | SS_CENTER, "X", 0, p.x - ship_width / 2, p.y - ship_height / 2, ship_width, ship_height);
-            current_position = p;
+            ship.create(*this, WS_CHILD | WS_VISIBLE | SS_CENTER, "X", 0, p.x, p.y, ship_width, ship_height);
         }
         else 
         {
             SetWindowPos(ship, 0, p.x, p.y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
-            current_position = p;
         }
+
+        current_position = p;
     }
 
     void on_key_up(int vk) override 
@@ -33,9 +33,12 @@ protected:
 
         if (is_moving) 
         {
-            SetWindowLong(ship, GWL_EXSTYLE, WS_EX_STATICEDGE);
+            LONG ex_style = GetWindowLong(ship, GWL_EXSTYLE);
+            ex_style = (ex_style & ~WS_EX_CLIENTEDGE) | WS_EX_STATICEDGE;
+            SetWindowLong(ship, GWL_EXSTYLE, ex_style);
             SetWindowPos(ship, 0, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
             is_moving = false;
+            SetWindowText(ship, "X");
         }
     }
 
@@ -49,7 +52,9 @@ protected:
 
         if (!is_moving) 
         {
-            SetWindowLong(ship, GWL_EXSTYLE, WS_EX_CLIENTEDGE);
+            LONG ex_style = GetWindowLong(ship, GWL_EXSTYLE);
+            ex_style = (ex_style & ~WS_EX_STATICEDGE) | WS_EX_CLIENTEDGE;
+            SetWindowLong(ship, GWL_EXSTYLE, ex_style);
             SetWindowPos(ship, 0, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
             is_moving = true;
         }
@@ -59,16 +64,20 @@ protected:
         switch (vk) 
         {
         case VK_LEFT:
-            current_position.x -= step;
+            current_position.x = max(current_position.x - step, 0);
+            SetWindowText(ship, "<");
             break;
         case VK_RIGHT:
-            current_position.x += step;
+            current_position.x = min(current_position.x + step, client_rect.right - ship_width);
+            SetWindowText(ship, ">");
             break;
         case VK_UP:
-            current_position.y -= step;
+            current_position.y = max(current_position.y - step, 0);
+            SetWindowText(ship, "A");
             break;
         case VK_DOWN:
-            current_position.y += step;
+            current_position.y = min(current_position.y + step, client_rect.bottom - ship_height);
+            SetWindowText(ship, "V");
             break;
         default:
             return;
